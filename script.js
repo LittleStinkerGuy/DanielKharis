6440d186dbeb6bec9f21b431ddb4bc94d42f4408
Daniel Kharis <72678136+LittleStinkerGuy@users.noreply.github.com>
2024-02-20 14:21:56 -0800

    Added Backspace

--- script.js
+++ script.js
@@ -7,78 +7,77 @@ const password = "iAmTheTruestStinker";
 let curLine = "";
 
 if (getCookie("loggedIn")) {
-  allowAccess();
+    allowAccess();
 }
 
-document.addEventListener("keydown", function (event) {
-  if (event.code == "Space" && event.target == document.body) {
-    event.preventDefault();
-  }
-  if (event.key.length == 1) {
-    blink.style.opacity = 1;
-    text.innerHTML += event.key;
-    curLine += event.key;
-  } else if (event.code == "Enter") {
-    if (curLine == password) {
-      correctPassword();
-    } else {
-      wrongPassword();
-    }
-  }
-});
+document.addEventListener("keydown", handleKeyDown);
 setInterval(function () {
-  blink.style.opacity = blink.style.opacity == 0 ? 1 : 0;
+    blink.style.opacity = blink.style.opacity == 0 ? 1 : 0;
 }, 450);
 
-function numberButton(key = "") {
-  key = String(key);
-  if (key.length == 1 && !isNaN(key)) {
-    document.dispatchEvent(new KeyboardEvent("keydown", { key: key }));
-  }
-}
-
 function correctPassword() {
-  allowAccess();
-  setCookie("loggedIn", "true", 1);
-  document.cookie = `passwordEntered=true; expires=Thu, 18 Dec 2013 12:00:00 UTC`;
+    allowAccess();
+    setCookie("loggedIn", "true", 1);
+    document.cookie = `passwordEntered=true; expires=Thu, 18 Dec 2013 12:00:00 UTC`;
 }
 
 function allowAccess() {
-  mainContent.classList.remove("hidden");
-  passwordField.classList.add("hidden");
-  curLine = "";
+    mainContent.classList.remove("hidden");
+    passwordField.classList.add("hidden");
+
+    curLine = "";
 }
 
 function wrongPassword() {
-  passwordField.scrollIntoView(false);
-  text.innerHTML +=
-    '<br><strong style="color: red;">Invalid Pasword</strong><br>Enter Password:&nbsp;';
-  curLine = "";
+    passwordField.scrollIntoView(false);
+    text.innerHTML += '<br><strong style="color: red;">Invalid Pasword</strong><br>Enter Password:&nbsp;';
+    curLine = "";
 }
 
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let cookieList = decodedCookie.split(";");
-  for (let i = 0; i < cookieList.length; i++) {
-    let c = cookieList[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieList = decodedCookie.split(";");
+    for (let i = 0; i < cookieList.length; i++) {
+        let c = cookieList[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
 
 function superSecretButton() {
-  setCookie("loggedIn", "true", -1);
+    setCookie("loggedIn", "true", -1);
+}
+
+function handleKeyDown(event) {
+    if (event.key == "Space" && event.target == document.body) {
+        event.preventDefault();
+    }
+    if (event.key.length == 1) {
+        blink.style.opacity = 1;
+        text.innerHTML += event.key;
+        curLine += event.key;
+    } else if (event.key == "Enter" && curLine != "") {
+        if (curLine == password) {
+            correctPassword();
+        } else {
+            wrongPassword();
+        }
+    } else if (event.key == "Backspace" && curLine.length > 0) {
+        text.innerHTML = text.innerHTML.slice(0, -1);
+        curLine = curLine.slice(0, -1);
+        console.log("hi");
+    }
 }
